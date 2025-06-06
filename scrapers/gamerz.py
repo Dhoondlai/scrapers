d66d563fd5555321c633c7cba29f7fd484c8af46
okHadi <khan.hadi2951@gmail.com>
2025-06-06 20:22:27 +0500

    feat: add gpu category in gamerz.pk

--- scrapers/gamerz.py
+++ scrapers/gamerz.py
@@ -21,13 +21,36 @@ DATA = []
 
 def run(event, context):
     print("Starting" + vendor + " scraper")
-    urls = get_links("processor-2/")
 
-    print("URLs fetched: ", urls)
-    print('Total links:', len(urls))
+    categories = {
+        "Processor": "processor-2/",
+        "GPU": "gpu/",
+    }
 
-    scrape_data(urls)
+    urls_dict = {}
+
+    for db_category, url_category in categories.items():
+        try:
+            print("Calling get_links")
+            urls = get_links(url_category)
+            urls_dict[db_category] = urls
+        except Exception as e:
+            print(e)
+            exit(1)
+    print(urls_dict)
+    print('Total links:', sum(len(v) for v in urls_dict.values()))
+
+    scrape_data(urls_dict)
+    print("Total products scraped: ", len(DATA))
     insert_into_db(client, DATA, vendor)
+
+    # urls = get_links("processor-2/")
+
+    # print("URLs fetched: ", urls)
+    # print('Total links:', len(urls))
+
+    # scrape_data(urls)
+    # insert_into_db(client, DATA, vendor)
 
 
 def get_links(category):
@@ -47,50 +70,50 @@ def get_links(category):
     return urls
 
 
-def scrape_data(urls):
-    category = "Processor"
-    for link in urls:
-        print("Fetching data from: ", link)
-        # Fetch the page content
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0'}
+def scrape_data(url_dict):
+    for category, urls in url_dict.items():
+        for link in urls:
+            print("Fetching data from: ", link)
+            # Fetch the page content
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0'}
 
-        page = requests.get(link, headers=headers)
-        soup = BeautifulSoup(page.content, "html.parser")
+            page = requests.get(link, headers=headers)
+            soup = BeautifulSoup(page.content, "html.parser")
 
-        name = soup.find(
-            "h1", class_="product_title entry-title").text.strip()
+            name = soup.find(
+                "h1", class_="product_title entry-title").text.strip()
 
-        try:
-            price = soup.find(
-                "p", class_="price").find("span").find("bdi").text
+            try:
+                price = soup.find(
+                    "p", class_="price").find("span").find("bdi").text
 
-        except:
-            print("Price not found")
-            continue
+            except:
+                print("Price not found")
+                continue
 
-        warranty = soup.find("li", text=re.compile(r'Warranty'))
-        if warranty == None:
-            warranty = soup.find("li", text=re.compile(r'months'))
+            warranty = soup.find("li", text=re.compile(r'Warranty'))
             if warranty == None:
-                warranty = "Not Available"
+                warranty = soup.find("li", text=re.compile(r'months'))
+                if warranty == None:
+                    warranty = "Not Available"
+                else:
+                    warranty = warranty.text
             else:
                 warranty = warranty.text
-        else:
-            warranty = warranty.text
 
-        in_stock = soup.find("span", class_="availability-text").text
-        if "in stock" in in_stock.lower():
-            in_stock = True
-        else:
-            in_stock = False
+            in_stock = soup.find("span", class_="availability-text").text
+            if "in stock" in in_stock.lower():
+                in_stock = True
+            else:
+                in_stock = False
 
-        # print vars
-        print("=================Uncleaned data====================")
-        print_variables(name=name, vendor=vendor, price=price,
-                        warranty=warranty, category=category, link=link, in_stock=in_stock)
+            # print vars
+            print("=================Uncleaned data====================")
+            print_variables(name=name, vendor=vendor, price=price,
+                            warranty=warranty, category=category, link=link, in_stock=in_stock)
 
-        clean_data(name, vendor, price, warranty, category, link, in_stock)
+            clean_data(name, vendor, price, warranty, category, link, in_stock)
 
 
 def clean_data(name, vendor, price, warranty, category, link, in_stock):
